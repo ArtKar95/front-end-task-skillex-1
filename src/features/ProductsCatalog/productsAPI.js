99d70b637fc4b832733a0422285640e61f3dfa6d
ArtKar95 <artkarapetyan1995@gmail.com>
2025-09-15 17:14:17 +0400

    feat: add paginated products listing with API and show more button

--- src/features/ProductsCatalog/productsAPI.js
+++ src/features/ProductsCatalog/productsAPI.js
@@ -2,17 +2,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import products from '@/mockData/products.json';
 
 //!I wrote this delay function to simulate fetch
-const simulateFetch = async (data, delay = 500) =>
-  new Promise((resolve) => setTimeout(() => resolve(data), delay));
+const simulateFetch = async (delay = 500) =>
+  new Promise((resolve) => setTimeout(() => resolve(), delay));
 
 export const productsApi = createApi({
   reducerPath: 'productsApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
   endpoints: (builder) => ({
     getProducts: builder.query({
-      queryFn: async () => {
-        const data = await simulateFetch(products);
-        return { data };
+      queryFn: async ({ limit } = {}) => {
+        const dataSlice = products.slice(0, limit);
+        await simulateFetch();
+        return { data: { products: dataSlice, total: products.length } };
       },
     }),
   }),
