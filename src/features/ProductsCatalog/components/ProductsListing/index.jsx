7526e32bf5bdcb3b6ba4524b6bcd83e401d81d99
ArtKar95 <70438665+ArtKar95@users.noreply.github.com>
2025-09-15 17:25:05 +0400

    feat: add paginated products listing with API and show more button (#8)

--- src/features/ProductsCatalog/components/ProductsListing/index.jsx
+++ src/features/ProductsCatalog/components/ProductsListing/index.jsx
@@ -1,10 +1,26 @@
+import { useState, useMemo } from 'react';
 import { useGetProductsQuery } from '@/features/ProductsCatalog/productsAPI';
 import ProductCard from '../ProductCard';
 import ProductsSkeleton from '../ProductsSceleton';
 import './index.scss';
 
+const PAGE_SIZE = 12;
+
 const ProductsListing = () => {
-  const { data, isLoading } = useGetProductsQuery();
+  //! Of cours better to do with real API query params and pagination and filtring and sorting
+  //! I just simulating it this way for the task
+  const [limit, setLimit] = useState(PAGE_SIZE);
+
+  const { data, isFetching, isLoading } = useGetProductsQuery({ limit });
+  const { products = [], total = 0 } = data || {};
+
+  const handleLoadMore = () => {
+    setLimit((prev) => prev + PAGE_SIZE);
+  };
+
+  const hasMore = useMemo(() => {
+    return products?.length < total;
+  }, [products, total]);
 
   return (
     <div className='products'>
@@ -12,11 +28,23 @@ const ProductsListing = () => {
         {isLoading ? (
           <ProductsSkeleton />
         ) : (
-          data?.map((product) => (
+          products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))
         )}
       </div>
+
+      {!hasMore ? (
+        <div className='products__no-more'>
+          <p>🎉 You've reached the end of the list!</p>
+        </div>
+      ) : (
+        <div className='products__load-more'>
+          <button onClick={handleLoadMore} disabled={isFetching}>
+            {isFetching ? 'Loading...' : 'Load More'}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
